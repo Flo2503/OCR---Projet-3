d162a3ddcd1d53e4c96641df27bc3f975e44d8e3
Flo2503 <bruel.florent@gmail.com>
2018-07-22 21:59:48 +0200

    Add func to display characters's details

--- videoGame/main.swift
+++ videoGame/main.swift
@@ -2,6 +2,7 @@ class Main {
     var namePlayerOne = [Any]()
     var nameplayerTwo = [Any]()
     
+    
     func firstPlayer() {
         print("Hello ! "
             + "\nPlease player one enter your name: ")
@@ -20,10 +21,27 @@ class Main {
         }
     }
     
+    func skillsCharacters() {
+        print("Characters : "
+            + "\n"
+            + "\nFighter : The classic attacker, a good warrior !"
+            + "\nLife : 100, Weapon : Sword(Damage 10)"
+            + "\n"
+            + "\nMage: His talent ? Heal the members of his team. "
+            + "\nLife : 80, Weapon : Fire stick(Damage 8)"
+            + "\n"
+            + "\nColossus : Imposing and very resistant, but it will not hurt you badly."
+            + "\nLife : 200, Weapon : Mace(Damage 5)"
+            + "\n"
+            + "\nDarwf : His ax will deal you a lot of damage, but he does not have a lot of health."
+            + "\nLife : 50, Weapon : Ax(Damage 15)")
     }
+    
+    
 }
 
 
 var start = Main()
 start.firstPlayer()
 start.secondPlayer()
+start.skillsCharacters()

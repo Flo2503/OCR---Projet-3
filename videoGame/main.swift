cbd38bd6cbb92d6b3d9a72cf3cd4d5098b48a109
Flo2503 <bruel.florent@gmail.com>
2018-07-26 22:16:47 +0200

    changed func to create player. Used only one func

--- videoGame/main.swift
+++ videoGame/main.swift
@@ -1,47 +1,44 @@
 class Main {
+
     var playerOne: Player?
     var playerTwo: Player?
     
-    
-    func firstPlayer() {
-        print("Hello ! "
-            + "\nPlease player one enter your name: ")
-        if let playerOneName = readLine() {
-            playerOne = Player(name: playerOneName)
-            print("\(playerOne!.name) is the player one !")
-            
+    func createPlayer() -> Player {
+        print("Please enter your name :")
+        if let name = readLine() {
+            return Player(name: name)
+        }else {
+            print("Incorect name, please enter a valid name !")
+            return createPlayer()
         }
     }
     
-    func secondPlayer() {
-        print("Please player two enter your name: ")
-        if let playerTwoName = readLine() {
-            playerTwo = Player(name: playerTwoName)
-            print("\(playerTwo!.name) is the player one !")
-        }
+    func start() {
+        playerOne = createPlayer()
+        playerTwo = createPlayer()
     }
+    
     
     func skillsCharacters() {
         print("Characters : "
             + "\n"
             + "\nFighter : The classic attacker, a good warrior !"
-            + "\nLife : 100, Weapon : Sword(Damage 10)"
+            + "\nLife: 100, Weapon: Sword)"
             + "\n"
             + "\nMage: His talent ? Heal the members of his team. "
-            + "\nLife : 80, Weapon : Fire stick(Damage 8)"
+            + "\nLife: 80, Weapon: Fire Stick)"
             + "\n"
-            + "\nColossus : Imposing and very resistant, but it will not hurt you badly."
-            + "\nLife : 200, Weapon : Mace(Damage 5)"
+            + "\nColossus: Imposing and very resistant, but it will not hurt you badly."
+            + "\nLife: 200, Weapon: Mace)"
             + "\n"
-            + "\nDarwf : His ax will deal you a lot of damage, but he does not have a lot of health."
-            + "\nLife : 50, Weapon : Ax(Damage 15)")
+            + "\nDarwf: His ax will deal you a lot of damage, but he does not have a lot of health."
+            + "\nLife: 50, Weapon: Ax")
     }
     
     
 }
 
+Main().start()
 
-var start = Main()
-start.firstPlayer()
-start.secondPlayer()
-start.skillsCharacters()
+
+
--- videoGame/player.swift
+++ videoGame/player.swift
@@ -1,9 +1,9 @@
-class Player {
-    let name: String
-    let team = [Character]()
-    
-    init(name: String) {
-        self.name = name
-    }
-    
-}
+//
+//  player.swift
+//  videoGame
+//
+//  Created by Flo on 25/07/2018.
+//  Copyright © 2018 Flo. All rights reserved.
+//
+
+import Foundation

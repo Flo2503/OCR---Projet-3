d4f4c453e8dc474b7272385c886f430d934f5a3b
Flo2503 <bruel.florent@gmail.com>
2018-07-28 19:31:32 +0200

    created swtch to choose character and creatd funcs to name charcters

--- videoGame/main.swift
+++ videoGame/main.swift
@@ -1,5 +1,5 @@
 class Main {
-
+    
     var playerOne: Player?
     var playerTwo: Player?
     
@@ -38,16 +38,80 @@ class Main {
         
     }
     
-    func chooseCharacters() {
-        print("Choose a character !"
-        + "\n1. Fighter"
-        + "\n2. Mage"
-        + "\n3. Colossus"
-        + "\n4. Darwf")
+    
+    func chooseCharacters() -> Character  {
+        print("Choose a character: "
+            + "\n1. Fighter"
+            + "\n2. Mage"
+            + "\n3. Colossus"
+            + "\n4. Darwf")
         
-        
+        if let choice = readLine() {
+            switch choice {
+            case"1":
+                print("You added Fighter to your team")
+                nameFighter()
+                return Fighter(fighterName: "")
+            case"2":
+                print("You added Mage to your team")
+                nameMage()
+                return Mage(mageName: "")
+            case"3":
+                print("You added Colossus to your team")
+                nameColossus()
+                return Colossus(colossusName: "")
+            case"4":
+                print("You added Darwf to your team")
+                nameDarwf()
+                return Darwf(darwfName: "")
+            default:
+                print("Incorect choice. Please choose a character: ")
+                return chooseCharacters()
+            }
+            
+        }
+        return chooseCharacters()
     }
     
+    func nameFighter() -> Fighter {
+        print("Please name your character: ")
+        if let name = readLine() {
+            return Fighter(fighterName: name)
+        }else {
+            print("Incorect name, please enter a valid name !")
+            return nameFighter()
+        }
+    }
+    
+    func nameMage() -> Mage {
+        print("Please name your character: ")
+        if let name = readLine() {
+            return Mage(mageName: name)
+        }else {
+            print("Incorect name, please enter a valid name !")
+            return nameMage()
+        }
+    }
+    
+    func nameColossus() -> Colossus {
+        print("Please name your character: ")
+        if let name = readLine() {
+            return Colossus(colossusName: name)
+        }else {
+            print("Incorect name, please enter a valid name !")
+            return nameColossus()
+        }
+    }
+    
+    func nameDarwf() -> Darwf {
+        print("Please name your character: ")
+        if let name = readLine() {
+            return Darwf(darwfName: name)
+        }else {
+            print("Incorect name, please enter a valid name !")
+            return nameDarwf()
+        }
+    }
     
 }
 
@@ -55,4 +119,3 @@ Main().start()
 var characters = Main()
 characters.displayCharacters()
 characters.chooseCharacters()
-

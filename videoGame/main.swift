eb0db21539c3d82c94f07077302361a95eb7ca9f
Flo2503 <bruel.florent@gmail.com>
2018-08-24 16:44:01 +0200

    Added more details to comments

--- videoGame/main.swift
+++ videoGame/main.swift
@@ -3,7 +3,8 @@ class Main {
     var playerOne: Player?
     var playerTwo: Player?
     
-// Ask name to the players and check if names are valid.
+/* Get the name of the users. Call func "isValid" and returns a player if name is correct (if name is not empty and if name doesn't already exists).
+     If conditions are not respected, the method returns an error message and restarts. */
     func createPlayer() -> Player {
         if let name = readLine(), isValid(name: name) {
             return Player(name: name)
@@ -13,7 +14,7 @@ class Main {
         }
     }
 
-// Launch the game and create players alternately.
+// Start the game and create players alternately. Player One calls createPlayer() then the player Two.
     func createPlayers() {
         print("Hello !"
         + "\nPlayer One enter your name:")
@@ -23,7 +24,7 @@ class Main {
     }
     
     
-// Display characters's details.
+// Display details of each character. Call static property in each character class (defaultIcon, and so on...).
     func displayCharacters() {
         print("Characters : "
             + "\n"
@@ -43,15 +44,13 @@ class Main {
     }
     
 
-// Allows players to choose their characters and name them.
+/* Display choices to the users allows them to choose characters. The method get the answers of the users and returns a character. Then this method calls nameCharacter() in order to name the chosen character. If choice is not valid, the method returns an error message and restarts. */
     func chooseCharacters() -> Character  {
-        print("\n1. Fighter"
-            + "\n2. Mage"
-            + "\n3. Colossus"
-            + "\n4. Dwarf")
-            
-        
-        if let choice = readLine() {
+        print("\n1. Fighter \(Fighter.defaultIcon)"
+            + "\n2. Mage \(Mage.defaultIcon)"
+            + "\n3. Colossus \(Colossus.defaultIcon)"
+            + "\n4. Dwarf \(Dwarf.defaultIcon)")
+            if let choice = readLine() {
             switch choice {
             case"1":
                 return Fighter(fighterName: nameCharacter())
@@ -71,7 +70,7 @@ class Main {
     }
     
     
-// Get characters's names given by the user. Also checks if names are valid. The method is called in chooseCharacters().
+/* Get characters's names given by the user. The method calls "isValid()" to check if name is not empty and if name doesn't already exists. If name is correct the method returns String, else it returns an error message and restarts. */
     func nameCharacter() -> String {
         print("Please name your character: ")
         if let name = readLine(), isValid(name: name) {
@@ -82,15 +81,16 @@ class Main {
         }
     }
     
-// Method to check if the name is not empty and if the names given by the user doesn't already exist.
+/* Method wich checks if name is valid. Parameter called "name": type String. The metod returns true if name is not empty and if names already
+     exists. The property nameList calls the method getNames() and gets each player in parameter. */
     func isValid(name: String) -> Bool {
         var nameList = getNames(player: playerOne)
         nameList.append(contentsOf: getNames(player: playerTwo))
-        
         return !name.isEmpty && !nameList.contains(name)
     }
     
-// Gets names and add them in nameList.
+
+/* Method to avoid duplicate code in isValid() method. Parameter called player, type : Player. The method retunrs an array of String. Names of players and characters are stored in nameList array. A "for" loop is used to get names in "team" array of players. Then the're checked with the method isValid(). */
     func getNames(player: Player?) -> [String] {
         var nameList = [String]()
         
@@ -103,18 +103,19 @@ class Main {
         return nameList
     }
 
-// Loop allowing each player to choose and name characters alternately.
+/* Loop allowing each player to choose and name characters alternately. The "for" loop is runig until three characters are chosen by each user. Chosen characters are stored in "team" array of each players calling choosenCharacters() method allowing to display choices. */
     func populateTeam() {
         for i in 0...2 {
             print("Player one choose a character: ")
-            playerOne?.team.append(main.chooseCharacters())
+            playerOne?.team.append(chooseCharacters())
             print("Player two choose a character: ")
-            playerTwo?.team.append(main.chooseCharacters())
+            playerTwo?.team.append(chooseCharacters())
         }
         
     }
 
-// Displays name of each player, also display details of each character in team.
+
+/* The method gets Player in parameter. A loop "for" is used to iterate over "team" array of each players allowing to display details (names, icons, and life points) of each characters in teams. */
     func playerTeam(player: Player?) {
         print("\n"
             + "\n\(player!.name) ! Your team is composed by the following characters :")
@@ -125,45 +126,40 @@ class Main {
         }
     }
     
+
+// Method allowing to call each player for the method "playerTeam()".
     func displayTeam() {
         playerTeam(player: playerOne)
         playerTeam(player: playerTwo)
     }
  
-    
+
+/* Method wich gets a player in parameter allowing to alternate player one and player two. It allows for each players to select a character in their own team in order to attack. The loop "for" iterates over players's team displaying an index for each characters, usefull for the users in order to select the character. The loop only displays the living characters calling isAlive() method. Then the method gets the user's answer. "Int(characterIndex)" -> readline() throws a string, so we switch it in Int. "player.team.indices.contains(index)" -> We check if the answer of the user is in the index. Then the choice of the user is stored in "character" property if the character is alive. If the answer of the user in incorect or if the character is dead, the method print an error message and returns the method. */
     func selectCharacter(player: Player) -> Character {
         for (index, character) in player.team.enumerated() {
             if character.isAlive() {
               print(index, ":", character.icon + " " + character.name)
             }
         }
-        
         if let characterIndex = readLine() {
             if let index = Int(characterIndex), player.team.indices.contains(index) {
               let character = player.team[index]
                 if character.isAlive() {
                     print("You selected \(character.name)")
                     return character
-                    
                 }else {
                     print("This character is dead, choose an other one: ")
                     return selectCharacter(player: player)
                 }
-                
-            
-            }else {
-                print("Incorect choice, please select a character: ")
-                return selectCharacter(player: player)
             }
-            
-            
         }
+        print("Incorect choice, please select a character: ")
         return selectCharacter(player: player)
     }
 
-    
-
 }
+
+
 
 //Main's instance
 let main = Main()
@@ -173,7 +169,7 @@ main.populateTeam()
 main.displayTeam()
 
 
-
+/* Loop "while" allowing to each players to attack or treat alternately. It runs as long as each team has a character alive, calling method "hasACharacterAlive()" for each player. The loop calls "selectCharacter()" allowing to the user to select an attcker in this team. Loop checks if character is of type Mage (thanks to "if _ is" allowing to check the type of an object). If character is a mage victime displays the team of the player to treat a character, otherwise victim displays the team opponent to attack. */
 while main.playerOne!.hasACharacterAlive() && main.playerTwo!.hasACharacterAlive() {
     print("\(main.playerOne!.name) choose an attacker !")
     var attacker = main.selectCharacter(player: main.playerOne!)
@@ -185,8 +181,6 @@ while main.playerOne!.hasACharacterAlive() && main.playerTwo!.hasACharacterAlive
         print("Now choose your victim !")
         victim =  main.selectCharacter(player: main.playerTwo!)
     }
-    
-    
     attacker.attack(victim: victim)
     print("\(victim.icon + " " + victim.name) has \(victim.lifePoint) lifes point left")
     print("\(main.playerTwo!.name) choose an attacker !")
